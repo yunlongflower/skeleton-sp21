7739c8052e3017e7e1949e7077a8d09ac1ddd524
yunlongflower <yunlongflower@163.com>
2024-09-17 14:38:46 +0800

    proj1: MaxArrayDeque API check

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,28 +3,10 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
-    private T maxValue;
     private Comparator<T> comparator;
     public MaxArrayDeque(Comparator<T> c) {
         super();
-        maxValue = null;
         comparator = c;
-    }
-
-    @Override
-    public void addFirst(T item) {
-        if (isEmpty() || comparator.compare(item, max_value) > 0) {
-            maxValue = item;
-        }
-        super.addFirst(item);
-    }
-
-    @Override
-    public void addLast(T item) {
-        if (isEmpty() || comparator.compare(item, max_value) > 0) {
-            maxValue = item;
-        }
-        super.addLast(item);
     }
 
     private T findMax(Comparator<T> c) {
@@ -38,25 +20,8 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> {
         return max;
     }
 
-    @Override
-    public T removeFirst() {
-        T result = super.removeFirst();
-        if (result == maxValue) {
-            maxValue = findMax(comparator);
-        }
-        return result;
-    }
-
-    public T removeLast() {
-        T result = super.removeLast();
-        if (result == maxValue) {
-            maxValue = findMax(comparator);
-        }
-        return result;
-    }
-
     public T max() {
-        return maxValue;
+        return findMax(comparator);
     }
 
     public T max(Comparator<T> c) {
